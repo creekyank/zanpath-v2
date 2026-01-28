d68a57d6635d3850b5c96e6235fe88d04290b234
creekyank <creekyank@gmail.com>
2026-01-28 18:03:10 +0800

    fix: correct i18n path in next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 // 🟢 告诉插件你的配置文件在哪里
-const withNextIntl = createNextIntlPlugin("./i18n.ts");
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
   /* 这里保留你原本的配置，比如 images, rewrites 等 */
