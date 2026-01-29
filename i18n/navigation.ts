d15b265788c76e10f14818c9e539d5943f921959
creekyank <creekyank@gmail.com>
2026-01-29 09:30:27 +0800

    save changes before merging to main

--- i18n/navigation.ts
+++ i18n/navigation.ts
@@ -9,7 +9,7 @@ export const routing = defineRouting({
   // 預設語言保持為英語
   defaultLocale: 'en',
   // 如果訪問不存在的語言，是否自動跳轉到預設語言
-  localePrefix: 'as-needed' 
+  localePrefix: 'always' 
 });
 
 // 2. 使用新版本的 createNavigation 导出组件
