163f6fd66a7757fa004182226759b45dbed0d0c4
creekyank <creekyank@gmail.com>
2026-03-31 12:34:12 +0800

    增加文章

--- app/[locale]/quiz/[type]/page.tsx
+++ app/[locale]/quiz/[type]/page.tsx
@@ -2,7 +2,8 @@
 "use client";
 
 import { useState } from "react";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
+import { useRouter } from "@/i18n/navigation";
 
 const questions: any = {
   personality: [
